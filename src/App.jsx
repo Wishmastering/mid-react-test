23720dd8bd99dcb42065ce81bf52ea53bf51796d
Wishmastering <charsdb@gmail.com>
2023-08-07 14:24:10 -0600

    responsive grid

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,8 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 import "./App.css";
 
 import useMovies from "./hooks/useMovies";
-
-function useSearch() {
-  const [search, setSearch] = useState("");
-  const [error, setError] = useState(null);
-
-  useEffect(() => {
-    if (search === "") {
-      setError("No se puede buscar una pelicula vacia");
-      return;
-    }
-
-    if (search.match(/^\d+$/)) {
-      setError("No se puede buscar una pelicula solo con numeros");
-      return;
-    }
-
-    if (search.length < 3) {
-      setError("La pelicula debe tener al menos 3 caracteres");
-      return;
-    }
-
-    setError(null);
-  }, [search]);
-
-  return { search, setSearch, error };
-}
+import useSearch from "./hooks/useSearch";
 
 export default function App() {
   const { movies } = useMovies();
@@ -87,16 +62,14 @@ export default function App() {
 
 function RenderMovies({ movies }) {
   return (
-    <div>
-      <ul>
-        {movies.map((movie) => (
-          <li key={movie.id}>
-            {movie.title} ({movie.year})
-            <img src={movie.poster} alt={`${movie.title} poster image`} />
-          </li>
-        ))}
-      </ul>
-    </div>
+    <ul className="movies">
+      {movies.map((movie) => (
+        <li className="movie" key={movie.id}>
+          {movie.title} ({movie.year})
+          <img src={movie.poster} alt={`${movie.title} poster image`} />
+        </li>
+      ))}
+    </ul>
   );
 }
 
